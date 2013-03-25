df09b982a8766d50cf8f757358fa9a740c0839a7
josuehernandez <joosuee.jv@gmail.com>
2013-03-25 00:52:18 -0600

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,28 @@
+/*
+
+< menor
+
+> mayor
+<= menor o igual
+
+>= mayor ó igual
+
+== identico
+
+1.- Calcula si el numero a es mayor al numero b.
+a = 19 , b= 17
+*/
+
+a = 19 ;
+
+b = 17 ;
+
+if( a > b ){
+ 
+console.log("si es mayor")
+
+}else{
+
+console.log("no es mayor")
+
+}
